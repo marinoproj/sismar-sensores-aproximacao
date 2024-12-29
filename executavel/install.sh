28ff253a1c399246f68fc592645c727d1c39c47b
Leonardo Barbosa <leobar1995@gmail.com>
2024-12-29 16:42:50 -0300

    change sh

--- executavel/install.sh
+++ executavel/install.sh
@@ -2,12 +2,13 @@ sudo rm -f master.zip
 sudo rm -rf sismar-sensores-aproximacao-master/
 sudo wget https://github.com/marinoproj/sismar-sensores-aproximacao/archive/refs/heads/master.zip
 sudo unzip master.zip
-sudo cd sismar-sensores-aproximacao-master/
+cd sismar-sensores-aproximacao-master/
 sudo chmod 777 -Rf executavel/
 sudo mv executavel/ /opt/sismar_sensores
-sudo cd /opt/sismar_sensores
+cd ..
+sudo rm -f master.zip
+sudo rm -rf sismar-sensores-aproximacao-master/
+cd /opt/sismar_sensores
 sudo cp sismar_sensores.service /etc/systemd/system/sismar_sensores.service
 sudo /bin/systemctl daemon-reload
 sudo /bin/systemctl enable sismar_sensores.service
-sudo rm -f master.zip
-sudo rm -rf sismar-sensores-aproximacao-master/
--- executavel/update-version.sh
+++ executavel/update-version.sh
@@ -1,11 +1,30 @@
 sudo systemctl stop sismar_sensores.service
+
 sudo rm -f master.zip
 sudo rm -rf sismar-sensores-aproximacao-master/
+
 sudo wget https://github.com/marinoproj/sismar-sensores-aproximacao/archive/refs/heads/master.zip
+
 sudo unzip master.zip
-sudo rm sismar_sensores.jar
+
+sudo rm -f sismar_sensores.jar
 sudo cp sismar-sensores-aproximacao-master/executavel/sismar_sensores.jar .
-sudo chmod 777 -Rf sismar_sensores.jar
+
+sudo rm -f startup.html
+sudo cp sismar-sensores-aproximacao-master/executavel/startup.html .
+
+sudo rm -f start
+sudo cp sismar-sensores-aproximacao-master/executavel/start .
+
+sudo rm -f sismar_sensores.service
+sudo cp sismar-sensores-aproximacao-master/executavel/sismar_sensores.service .
+
+sudo rm -f update-version.sh
+sudo cp sismar-sensores-aproximacao-master/executavel/update-version.sh .
+
+sudo chmod 777 -Rf ./
+
 sudo rm -f master.zip
 sudo rm -rf sismar-sensores-aproximacao-master/
+
 sudo systemctl restart sismar_sensores.service
