f551a68280b2cc487f660500c86731d254858bdc
Leonardo Barbosa <leobar1995@gmail.com>
2025-01-03 09:00:31 -0300

    add script open sismar

--- executavel/open-sismar.sh
+++ executavel/open-sismar.sh
@@ -12,14 +12,20 @@ TARGET_URL="http://localhost:5000"
 # Abrir a página de carregamento no navegador padrão
 firefox "$LOADING_PAGE" &
 
-# Verificar status de saúde
 (
     while true; do
         # Fazer a requisição HTTP e capturar o status
         STATUS=$(curl -s "$HEALTH_CHECK_URL" | jq -r '.status')
 
-        # Se o status for UP, abrir a página de destino na mesma aba e sair
+        # Se o status for UP, fechar o Firefox de forma segura e abrir a página de destino
         if [ "$STATUS" == "UP" ]; then
+            # Fechar o Firefox de forma segura
+            pkill -f firefox
+
+            # Aguardar o Firefox fechar completamente
+            sleep 1
+
+            # Abrir a página de destino na mesma aba
             firefox --new-tab "$TARGET_URL"
             break
         fi
